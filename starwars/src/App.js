1570e50a983bc4754fe0a77d60b99460e9fcc6b1
MichaelHMods <mikeguadalupe@gmail.com>
2019-08-16 18:54:35 -0500

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,17 +2,39 @@ import React, { useState, useEffect} from 'react';
 import './App.css';
 import axios from "axios";
 import DataCard from "./Datacard";
+//Style
+import styled from "styled-components";
 
+
+const AppContainer = styled.div`
+    max-width: 1200px;
+    width: 100%;
+    
+    // border: 1px solid red;
+`
+const PageHeader = styled.h1 `
+    font-size: 4rem;
+    padding-left: 35%
+    `
+const PageHeader2 = styled.h1 `
+    font-size: 4rem;
+    padding-left: 10%
+    `
+const HeaderBox = styled.div `
+    width:100%
+    justify-content:center
+    border: 1px solid black
+    `
 
 function App() {
   
   const [rwState, setRWState] = useState();
     useEffect(() => {
       const fetchData = () => {
-        axios.get('https://swapi.co/api/people')
+        axios.get('https://swapi.co/api/people/')
         .then(response => {
-          const test = response.data
-          console.log(response.data)
+          const test = response.data.results
+          console.log(response.data.results)
           setRWState(test);
         })
         .catch(error => {
@@ -22,18 +44,19 @@ function App() {
       fetchData();
     },[])
         
-      console.log(rwState)        
+              
       if (!rwState) {return <h1>loading</h1>}
       else {
   
     return (
       
-      <div className="App">
-        <h1 className="Header">{rwState}</h1>
-        <p>May the force be with you.</p>
-        <DataCard/>
-
-      </div>
+      <AppContainer>
+        <HeaderBox>
+          <PageHeader>React Stars</PageHeader>
+          <PageHeader2>May the force be with you.</PageHeader2>
+        </HeaderBox>
+        {rwState.map((Character,index) => <DataCard key= {index} starName={Character}/>)}
+      </AppContainer>
      
     )};
     
--- starwars/src/Datacard.jsx
+++ starwars/src/Datacard.jsx
@@ -1,16 +1,42 @@
 import React from "react";
 import Apps from './App';
+import styled from "styled-components"
+
+const Card = styled.div`
+width: 90%;
+display:flex;
+justify-content: space-around;
+`
+const CardContainer = styled.div`
+    width: 35%;
+    border: 1px solid black;
+    margin: 4% 0;
+    background: gold;
 
 
-function DataCard() {
-    console.log(Apps)
+  `
+const CardHead = styled.h4`
+    font-size: 1.6rem;
+    padding 5%;
+    `
+const CardDetails = styled.h4`
+    font-size: 1.9rem;
+    padding 5%;
+    `
+
+function DataCard(props) {
+    console.log(props.starName)
     return (
-        <div className="container-card">
-            {/* <h4 className="name">{props.name}</h4>;
-            <p className="height">{props.height}</p>;
-            <p className="weight">{props.mass}</p>; */}
+        <Card>  
+            <CardContainer>
+                <CardHead>Name: {props.starName.name}</CardHead>
+                <CardDetails>Height:{props.starName.height}cm</CardDetails>
+                <CardDetails>Mass{props.starName.mass}kg</CardDetails>
 
-        </div>
+            </CardContainer>
+        </Card>
+    
+    
     )
 }
 
