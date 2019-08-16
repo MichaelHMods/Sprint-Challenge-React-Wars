27289b1f263563110f0315f7d375c33d26677262
MichaelHMods <mikeguadalupe@gmail.com>
2019-08-16 11:42:35 -0500

    cards recovered

---
+++ starwars/cardStuff/Data.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect} from 'react';
+import axios from "axios";
+import App from './App';
+
+function Data() {
+
+    console.log('Data', props.name)
+    // const StarCard = (props)
+    
+    
+  
+  }
+  export default Data
+  
--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import Data from './Data';
+
+import axios from "axios";
+
 
 const App = () => {
+  <Data/>
+  const [rwState, setRWState] = useState();
+    useEffect(() => {
+      const fetchData = () => {
+        axios.get('https://swapi.co/api/people')
+        .then(response => {
+          const test = response.data
+          console.log(response.data)
+          setRWState(test);
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      };
+      fetchData();
+    },[])
+        
+      console.log(rwState)        
+      if (!rwState) {return <h1>loading</h1>}
+      else {
+  
+    return (
+      <div className="App">
+      <h1 className="Header">React Wars</h1>
+      <p>Stuff is here</p>
+      </div>
+     
+    )};
+  }
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,11 +42,5 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
-}
 
 export default App;
